f66cc374ae5730cb85c6000bcc3bf1b32e7c66b9
Ayush Raj <ardubey1504@gmail.com>
2024-05-11 22:53:39 +0530

    done

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const dbConnection = ()=> {
-  mongoose.connect(process.env.MONGO_URI,
+  mongoose.connect("mongodb+srv://rajayush1504:ayush1234@cluster0.l4x8kdi.mongodb.net/?retryWrites=true",
   {
     dbName: "RESTURANT",
   }).then (() => {
